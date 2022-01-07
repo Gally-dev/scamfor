227dcac546dce9417e78198abc6eb02c13f552ad
Gally-dev <lukas.gally@gmail.com>
2022-01-07 20:26:02 +0100

    prva zmena

---
+++ src/components/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <label for="risk"> {{risk}}</label>
+  <input type="checkbox" name="risk" id="" v-for="risk in risks" :key="risk.id"/>
+    
+  
+</template>
+
+<script>
+
+export default {
+   
+    data() {
+        return {
+        risks: [
+            "upozornenie Národnej banky Slovenska, prípadne inej verejnej autority na aktivity takého subjektu",
+            "vedenie súdneho alebo administratívneho konania proti poskytovateľovi služieb",
+            "potreba zadať, zdieľať alebo komukoľvek poslať privátne kľúče, či heslo od peňaženky alebo účtu",
+            "poskytovateľ služby garantuje ziskovosť alebo návratnosť investície (osobitne v krátkom časovom horizonte)",
+            "použitie lákavých fráz „zarábajte online“ alebo „pasívny príjem“",
+            "veľkolepé tvrdenia o vlastnom kryptoaktíve „nový Bitcoin/lepší ako Bitcoin“ s garantovaním budúceho zhodnotenia",
+            "požiadavky na realizáciu ďalšej investície, pre účely výberu skoršej investície",
+            "požiadavky na zaslanie kryptoaktív na neznáme adresy peňaženiek alebo na neznáme adresy bankových účtov, ktoré nie sú asociované s poskytovateľom predmetných služieb",
+            ],
+        };
+  },
+};
+</script>
+
+<style lang="css" scoped>
+
+</style>
