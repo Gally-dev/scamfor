de87f4165820df1beada55ca3e8f6498b2939996
Gally-dev <lukas.gally@gmail.com>
2022-01-10 08:57:40 +0100

    data in data added

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 //
 /// RISKS
-///
+//
 export const data = [
   "upozornenie Národnej banky Slovenska, prípadne inej verejnej autority na aktivity takého subjektu",
   "vedenie súdneho alebo administratívneho konania proti poskytovateľovi služieb",
@@ -10,4 +10,14 @@ export const data = [
   "veľkolepé tvrdenia o vlastnom kryptoaktíve „nový Bitcoin/lepší ako Bitcoin“ s garantovaním budúceho zhodnotenia",
   "požiadavky na realizáciu ďalšej investície, pre účely výberu skoršej investície",
   "požiadavky na zaslanie kryptoaktív na neznáme adresy peňaženiek alebo na neznáme adresy bankových účtov, ktoré nie sú asociované s poskytovateľom predmetných služieb",
+  "prísľub spätného zaslania násobku kryptoaktív po zaslaní určitého množstva zo strany užívateľa",
+  "štruktúra poskytovateľa javí znaky MLM (multi-level marketingu) a odmeňovanie za používanie služby čiastočne závisí od schopností vytvorenia vlastnej predajnej siete",
+  "ponuky na poskytnutie služby alebo investovanie prostredníctvom nevyžiadanej e-mailovej komunikácie, prostredníctvom sociálnych sietí, prípadne nevyžiadaných telefonických hovorov",
+  "marketingová komunikácia založená na využívaní botov",
+  "absencia formálneho zmluvného dokumentu pri poskytovaní služieb",
+  "recenzie k produktu obsahujú zmienky o problémoch s výberom prostriedkov investície, prípadne proklamovaného zisku",
+  "vytvorenie a predaj vlastného kryptoaktíva bez existencie využitia (utility)a trhu na jeho budúce odpredanie",
+  "prevádzkovanie trading, signálových alebo Pump & Dump komunitných skupín (telegram, discord...)",
+  "poskytovanie služby prenájmu výpočtového výkonu za účelom ťažby kryptoaktív",
+  "súčasťou kryptoaktíva ponúkaného projektu je účasť v ICO (Initial Coin Offering), STO (Security Token Offering) alebo iného modelu na zhromažďovanie peňažných prostriedkov alebo kryptoaktív zo strany verejnosti",
 ];
