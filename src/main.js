5b71e7cf87f2dda79ab4cb273914d8ac04b547b7
Gally-dev <lukas.gally@gmail.com>
2022-01-14 09:10:13 +0100

    uninstall gtag from json

--- src/main.js
+++ src/main.js
@@ -1,21 +1,8 @@
-// import { createApp } from "vue";
-// import App from "./App.vue";
-
-
-// createApp(App).mount("#app");
-
-
-
 import { createApp } from "vue";
 import App from "./App.vue";
-import VueGtag from "vue-gtag-next";
 
 const app = createApp(App);
 
-app.use(VueGtag, {
-  property: {
-    id: "UA-217066122-1"
-  }
-});
+
 
 app.mount("#app");
