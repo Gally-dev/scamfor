57240cbc9fcd6498626cfdbc3fa9dd442147a795
Gally-dev <lukas.gally@gmail.com>
2022-01-14 08:53:42 +0100

    pridany VueGtag

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
+// import { createApp } from "vue";
+// import App from "./App.vue";
+
+
+// createApp(App).mount("#app");
+
+
+
 import { createApp } from "vue";
 import App from "./App.vue";
+import VueGtag from "vue-gtag-next";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(VueGtag, {
+  property: {
+    id: "UA-217066122-1"
+  }
+});
+
+app.mount("#app");
